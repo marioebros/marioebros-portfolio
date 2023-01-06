35a51fd09f2119dc9d75d4b82372d335499ffc71
Mario M. Elizalde <dollarize.mme@gmail.com>
2023-01-05 17:28:46 -0800

    Changeds processed thru Next.js pkge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bfde227ad9204f6d"; }
+/******/ 		__webpack_require__.h = function() { return "2b6f94c2790d317e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
