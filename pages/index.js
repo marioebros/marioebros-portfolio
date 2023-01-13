3ab59d27fa7b0c4ee1f3e1d944bac729562f6990
Mario M. Elizalde <dollarize.mme@gmail.com>
2023-01-12 17:33:21 -0800

    Added Social links and formatted bg + Image import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillInstagram,
+  AiFillGithub,
+} from "react-icons/ai";
+import Image from "next/image";
+import marioebros from "../public/marioebros.jpeg";
 
 export default function Home() {
   return (
@@ -14,7 +22,7 @@ export default function Home() {
       <main className="bg-white px-10">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 text-xl>Mario M. Elizalde</h1>
+            <h1 className="text-xl font-Playfair">DevelopedBymarioeBros</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
@@ -29,6 +37,22 @@ export default function Home() {
               </li>
             </ul>
           </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-orange-500 font-medium">
+              Mario M. Elizalde
+            </h2>
+            <h3 className="text-2xl py-2">Software Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">The Man.</p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-800">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillInstagram />
+            <AiFillGithub />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-orange-600 rounded-full w-80 h-80 mt-20">
+            <Image src={marioebros} />
+          </div>
         </section>
       </main>
     </div>
