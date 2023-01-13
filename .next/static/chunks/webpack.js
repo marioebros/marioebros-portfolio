2a2dd7951ea6b9a49c4ef1597cad618ee7a1d3ec
Mario M. Elizalde <dollarize.mme@gmail.com>
2023-01-12 16:19:23 -0800

    Update: npx tailwindcss init + npm run dev

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8241ff9b0b078566"; }
+/******/ 		__webpack_require__.h = function() { return "c0d4945efb78532b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
