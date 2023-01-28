e68dc2b4e46e913532d9abdec6a05f9985347833
Mario M. Elizalde <dollarize.mme@gmail.com>
2023-01-27 20:18:21 -0800

    npm run dev updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b40141391560d4a1"; }
+/******/ 		__webpack_require__.h = function() { return "f45b78d6fee1c573"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
